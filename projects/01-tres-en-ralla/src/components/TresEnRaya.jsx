2edd7581329192e0d220f64efc836604a0c9a940
David Ingles <david@MATRIX>
2025-03-05 18:13:50 +0100

    termino el tres en reya y funciona muy bien

--- projects/01-tres-en-ralla/src/components/TresEnRaya.jsx
+++ projects/01-tres-en-ralla/src/components/TresEnRaya.jsx
@@ -6,7 +6,7 @@ const TURNS = {
   O: 'O'
 }
 
-function Square({ index, turn, isActive, children, updateBoard }) {
+function Square({ index, isActive, children, updateBoard }) {
   const handleClick = () => {
     updateBoard(index)
   }
@@ -17,17 +17,48 @@ function Square({ index, turn, isActive, children, updateBoard }) {
   )
 }
 
+const WINNER_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function TresEnRaya() {
   const [turn, setTurn] = useState(TURNS.X)
   const [board, setBoard] = useState(Array(9).fill(null))
+  const [ winner, setWinner ] = useState(null)
 
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBINATIONS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
 
   const updateBoard = (indice) => {
+    if(board[indice] !== null || winner) return
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
     const newBoard = [...board]
     newBoard[indice] = turn  
     setBoard(newBoard)
+    checkWinner(newBoard)
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
@@ -39,8 +70,8 @@ function TresEnRaya() {
             <Square
               key={index}
               index={index}
-              turn={turn}
               updateBoard={updateBoard}
+              isActive={winner && (board[index] === winner)}
             >
               {board[index]}
             </Square>
